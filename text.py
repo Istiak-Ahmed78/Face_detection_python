c056e93be00f2a8f90d70cef71304f5c69ff9870
Istiak-Ahmed78 <aistiak2078@gmail.com>
2023-01-07 17:47:35 +0600

    person position sensitivity added

--- text.py
+++ text.py
@@ -7,7 +7,13 @@ faceCaseCade=cv2.CascadeClassifier(casPath)
 
 FRAME_W = 400
 FRAME_H = 300
-# To capture video from webcam. 
+
+
+CENTER_OF_H= FRAME_H/2
+CENTER_OF_W=FRAME_W/2
+
+CLOSE_HIGHT = 150
+CLOSE_WEIGHT = 150
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,  FRAME_W)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_H)
@@ -20,67 +26,86 @@ time.sleep(2)
 
 count=0
 
-
-def getFetch():
-
-    while True:
-    # Read the frame
-        f, img = cap.read()
-
-        if not f:
-            print('We are not receiving data anymore')
-            break
-        # Convert to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Detect the faces
-        faces = faceCaseCade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-        )
-
-        # Draw the rectangle around each face
-
-        #Face Square X axis
-        print(len(faces))
-        if len(faces) != 0:
-            (F_F_X,F_F_Y,F_F_W,F_F_H)=faces[0]
-            print('We got a face')
-            cv2.rectangle(img, (F_F_X, F_F_Y), (F_F_X+F_F_W, F_F_Y+F_F_H), (255, 0, 0), 2)
+def trackTheClosestFace(faces):
+    # Closest width
+    cWL=200
+    cWR=260
+    for (x, y, w, h) in faces:
+        if(x<cWL):
+            turnRight()
+        elif(x>cWR):
             
-            # Display
-            cv2.imshow('img', img)
-                # Stop if escape key is pressed
-            k = cv2.waitKey(30) & 0xff
-            if k==27:
-                break
-            # return faces[0]
+            turnLeft()
         else:
-            print('No face detected')
-            # break
-    # #Face square Y axis
-    # F_F_Y=faces[0].y
-    # #Width
-    # F_F_W=faces[0].w
-    # #Hieght
-    # F_F_H=faces[0].h
-# face=getFetch()
-# if  face is not None:
-#     x_center=getCenterOfAxis(F_F_X)
-#     print(x_center)
-
-def getFetch():
-    print('Be angry')
-
-
-
-
+            perform()
         
-# Release the VideoCapture object
+
+
+
+
+
+def perform():
+    print('Start greeting')
+
+def turnLeft():
+    print('Turn left')
+
+def turnRight():
+    print('Turn right')
+
+def isNotAtSide(x):
+    return x<260 and x>200
+# Is the person close and not at side of the robot
+def isClose(x,w,h):
+    return (h>CLOSE_HIGHT and w>CLOSE_WEIGHT) and (isNotAtSide(x))
+
+# Is pressed 'Esc' to close camera
+def isAppliedForClose():
+    k = cv2.waitKey(30) & 0xff
+    return k==27
+
+# Draw circle
+def drawR(img,x,y,w,h):
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+
+# detect faces
+def getFaces(gray):
+
+    faces = faceCaseCade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    return faces
+
+
+
+
+cap = cv2.VideoCapture(0)
+while True:
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces= getFaces(gray)
+    for (x, y, w, h) in faces:
+        drawR(img,x,y,w,h)
+        print('X-axis')
+        print(x)
+        print('Y-axis')
+        print(y)
+        print('Hieght')
+        print(h)
+        trackTheClosestFace(faces)
+        if(isClose(x,w,h)):
+            perform()
+    cv2.imshow('img', img)
+    if(isAppliedForClose()):
+        break
 cap.release()
+# Release the VideoCapture object
+
 
 
 
